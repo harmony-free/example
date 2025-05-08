6e80428a2047effd894c1dc61250f9de104b5b17
RogueYBJ <15179861454@163.com>
2025-05-08 15:53:27 +0800

    feat:主页面模块

--- entry/hvigorfile.ts
+++ entry/hvigorfile.ts
@@ -1,6 +1,20 @@
 import { hapTasks } from '@ohos/hvigor-ohos-plugin';
+import { HvigorPlugin, HvigorNode } from '@ohos/hvigor'
 
 export default {
     system: hapTasks,  /* Built-in plugin of Hvigor. It cannot be modified. */
-    plugins:[]         /* Custom plugin to extend the functionality of Hvigor. */
+    plugins:[
+        customPlugin()  // 应用自定义Plugin
+    ]         /* Custom plugin to extend the functionality of Hvigor. */
+}
+
+// 实现自定义插件
+function customPlugin(): HvigorPlugin {
+    return {
+        pluginId: 'customPlugin',
+        apply(node: HvigorNode) {
+            // 插件主体
+            console.log('hello customPlugin!');
+        }
+    }
 }
