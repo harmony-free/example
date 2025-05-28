fbec5ee98170c12cbb38fc4b53128a0d41df3097
RogueYBJ <15179861454@163.com>
2025-05-28 11:15:24 +0800

    feat:增加获取定位

--- entry/hvigorfile.ts
+++ entry/hvigorfile.ts
@@ -1,20 +1,10 @@
 import { hapTasks } from '@ohos/hvigor-ohos-plugin';
 import { HvigorPlugin, HvigorNode } from '@ohos/hvigor'
+import { genIndex, genModelPlugin, jsonToModelPlugin } from '@free/hvigor-arkts-gen-model';
 
 export default {
-    system: hapTasks,  /* Built-in plugin of Hvigor. It cannot be modified. */
+    system: hapTasks,  /* Built-in plugin of Hvigor. It cann ot be modified. */
     plugins:[
-        customPlugin()  // 应用自定义Plugin
+        // jsonToModelPlugin()
     ]         /* Custom plugin to extend the functionality of Hvigor. */
-}
-
-// 实现自定义插件
-function customPlugin(): HvigorPlugin {
-    return {
-        pluginId: 'customPlugin',
-        apply(node: HvigorNode) {
-            // 插件主体
-            console.log('hello customPlugin!');
-        }
-    }
 }
